8d79507a99ae7198cb04d5bd93e4dff04fc8d4b0
Amir Shayegh <shayegh@me.com>
2020-08-11 12:30:42 -0700

    created base observation form

---
+++ InvasivesBC/View Controllers/Geometry Picker/GeometryPickerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GeometryPickerViewController.swift
+//  InvasivesBC
+//
+//  Created by Amir Shayegh on 2020-08-11.
+//  Copyright © 2020 Government of British Columbia. All rights reserved.
+//
+
+import UIKit
+
+class GeometryPickerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ InvasivesBC/View Controllers/Plant Observation/Cells/SectionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SectionTableViewCell.swift
+//  InvasivesBC
+//
+//  Created by Amir Shayegh on 2020-08-11.
+//  Copyright © 2020 Government of British Columbia. All rights reserved.
+//
+
+import UIKit
+
+class SectionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
