0efef93393ab83a4517fd9d2eb37a6039933beef
Amir Shayegh <shayegh@me.com>
2020-08-12 12:07:09 -0700

    added plant observation model

---
+++ InvasivesBC/DataModels/Observation/ObservationFields.swift
@@ -0,0 +1,9 @@
+//
+//  ObservationFields.swift
+//  InvasivesBC
+//
+//  Created by Amir Shayegh on 2020-08-12.
+//  Copyright © 2020 Government of British Columbia. All rights reserved.
+//
+
+import Foundation
